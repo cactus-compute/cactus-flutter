7d834a70c57423fd8b04624fe520b73280078107
devabhi <devabhixda@gmail.com>
2025-09-29 11:58:47 -0700

    Expose rag

--- lib/cactus.dart
+++ lib/cactus.dart
@@ -2,6 +2,9 @@ library cactus;
 
 export 'models/types.dart';
 export 'models/tools.dart';
+export 'models/document.dart';
+export 'models/rag.dart';
 export 'services/lm.dart';
+export 'services/rag.dart';
 export 'services/telemetry.dart';
 export 'services/utility.dart';
